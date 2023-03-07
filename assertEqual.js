64f443a265d704780df59686ae4c7b74e2bb612c
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-07 09:15:46 -0700

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed");
+    console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
   } else if (actual !== expected) {
-    console.log("🛑🛑🛑 Assertion Failed");
+    console.log("🛑🛑🛑 Assertion Failed: " + [actual] + " !== " + [expected]);
   }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Light", "Light");
-assertEqual(1, "1");
+assertEqual(1, 2);
