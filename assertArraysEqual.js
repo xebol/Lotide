15bb0e42138c317b497bc05adc42fd84e76d60c5
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-08 09:54:59 -0700

    Updating assertEqual function and creating a function that checks if Arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = function (arr1, arr2) {
+  if(arr1.length !== arr2.length)
+   return false;
+ 
+  for(let i = 0; i < arr1.length; i++) {
+       if(arr1[i] === arr2[i]) {
+       } else {
+         return false;
+     }
+   }   return true;
+ };
