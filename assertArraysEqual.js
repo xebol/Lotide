bf060cef661f5b51289c165018612480bf05d185
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-08 13:52:04 -0700

    Updated assertArraysEqual and eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,23 @@
-const eqArrays = function (arr1, arr2) {
-  if(arr1.length !== arr2.length)
-   return false;
- 
-  for(let i = 0; i < arr1.length; i++) {
-       if(arr1[i] === arr2[i]) {
-       } else {
-         return false;
-     }
-   }   return true;
- };
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  //call eqArrays function in the assertArraysEqual function
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
--- eqArrays.js
+++ eqArrays.js
@@ -6,16 +6,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function (arr1, arr2) {
- if(arr1.length !== arr2.length)
-  return false;
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
 
- for(let i = 0; i < arr1.length; i++) {
-      if(arr1[i] === arr2[i]) {
-      } else {
-        return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
-  }   return true;
+  } return true;
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
