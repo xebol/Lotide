98e87ea5437f24bf5462fca781384e6cdcee431d
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-09 10:31:21 -0700

    Working with the last array in middle.js and creating a countOnly function, specifically counting items wanted

---
+++ countOnly.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (let i = 0; i < allItems.length; i++) {
+    if (itemsToCount[allItems[i]])
+      if (results[allItems[i]]) {
+        results[allItems[i]] += 1;
+      } else {
+        results[allItems[i]] = 1;
+      }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
---
+++ middle.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.ceil(array.length / 2 - 1)]];
+  }
+};
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
+console.log(middle([1, 2])); // => []
+console.log(middle([])); // => []
+console.log(middle(["middle"])); // => []
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
+console.log(middle(["Hello", "Lighthouse", "Labs"])); // => ["Lighthouse"]
+
+//Write test cases with assertArraysEqual
