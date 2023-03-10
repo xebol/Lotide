f2798b713d2384f779e52fe7cf928f3bdcee644a
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-10 10:08:16 -0700

    Implemented and function that finds the letter position in a sentence

--- letterPositions.js
+++ letterPositions.js
@@ -19,11 +19,48 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const letterPositions = function(sentence) {
-  const results = {};
-  // logic to update results here
-  return results;
+  const result = {};
+
+  //loop through the string 
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") //filter out the space
+      //check if the characted already exist in the result object
+      if (result[sentence[i]]) {
+        //if character exist, push it to the array
+        result[sentence[i]].push(i);
+      } else {
+        //if it does not exist currently, add the key to the object and assign value to the current index
+        result[sentence[i]] = [i];
+      }
+  }
+  return result;
 };
 
+console.log(letterPositions("lighthouse in the house"));
+console.log(letterPositions("abadakedavra"));
+console.log(letterPositions("hello"));
 
-//Test case
-assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house"), {
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+});
+
+assertArraysEqual(letterPositions("abadakedavra"), {
+  a: [0, 2, 4, 8, 11],
+  b: [1],
+  d: [3, 7],
+  k: [5],
+  e: [6],
+  v: [9],
+  r: [10]
+});
+
+assertArraysEqual(letterPositions("hello"), { h: [0], e: [1], l: [2, 3], o: [4] });
