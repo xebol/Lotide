8dcb5c502145ceda71696a25baf7c634b683be24
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-09 19:32:55 -0700

    Created a function that checks the corresponding key to a value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,22 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKeyByValue = function(object, value) {
+  for(let key in object) {
+
+  if(value === object[key]) {
+    return key;
+  }
+ } return undefined;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
