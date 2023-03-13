39d168510a8096763be746a9547704ee8aaaafb4
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-13 17:56:07 -0600

    Refactored headTest file. Included mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,24 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);//assertion pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //assertion pass
-assertEqual(head([2]), 2); //assertion pass
-assertEqual(head([]), 4); // assertion fails
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
