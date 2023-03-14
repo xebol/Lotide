99ec6dff3dbd81361d9b2705c8865c6bf70641c9
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-13 18:33:49 -0600

    Refactored middleTest. Added mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,24 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);// pass
-assertArraysEqual(middle([1, 2]), []); // pass
-assertArraysEqual(middle([]), []); // pass
-assertArraysEqual(middle(["middle"]), []); // pass
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // pass
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [3,4,5]); // fail
-assertArraysEqual(middle(["Hello", "Lighthouse", "Labs"]), ["Labs"]); // fail
+describe("#middle", () => {
+  it("returns ['2, 3'] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns ['2, 3'] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns an empty array if passed an array with two elements", () => {
+    assert.deepEqual(middle([1, 2]), [])
+  })
+
+  it("returns an empty array is passed an empty arra", () => {
+    assert.deepEqual(middle([]), [])
+  })
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  })
+});
