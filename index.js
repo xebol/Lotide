f0877f676e65a9905ab9f90192aa44b4818a1d28
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-03-13 19:00:52 -0600

    Added an index.js file to export all utility functions in lotide

---
+++ index.js
@@ -0,0 +1,13 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertArraysEqual');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual
+};
